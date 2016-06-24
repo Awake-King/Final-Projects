5082d3151ca18dd153e72c96e79c55f9ff63cc83
Awake-King <Ben.Auranos.Brown@gmail.com>
2016-06-24 15:24:33 -0600

    Add files via upload

---
+++ Finalish Project-1.cpp
@@ -0,0 +1,322 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+
+void enterSugar();
+void calcHemo();
+void calcSugar();
+void reviewSugar();
+void onlineInfo();
+
+using namespace std;
+int main(int argc, char *argv[]) 
+
+{
+
+	int userAnswer = 0;
+	int sugar;
+	int sumSugar;
+	int hemoglobin;
+
+
+
+	do
+	{
+		cout << "Please choose what you would like to do." << endl << endl;
+		cout << "1: Enter blood sugar." << endl;
+		cout << "2: Calculate hemoglobin A1C." << endl;
+		cout << "3: Calculate average blood sugar for all your entries." << endl;
+		cout << "4: Review your blood sugar entries." << endl;
+		cout << "5: To get more information online." << endl;
+		cout << "(-1 to stop the program)" << endl;
+
+
+		cin >> userAnswer;
+		cin.ignore(100, '\n');
+		cout << endl;
+
+		if(userAnswer == 1)
+		{
+			enterSugar();
+		}
+		else if (userAnswer == 2)
+		{
+			calcHemo();
+		}
+		else if(userAnswer == 3)
+		{
+			calcSugar();
+		}
+		else if(userAnswer == 4)
+		{
+			reviewSugar();
+		}
+		else if(userAnswer == 5)
+		{
+			onlineInfo();
+		}
+		else if(userAnswer == -1)
+		{
+			cout << "Thank you for using our program." << endl;
+		}
+		else
+			cout << "What you entered is not in our system." << endl;
+
+
+
+	} while(userAnswer != -1);
+
+
+
+
+	return 0;
+}
+
+void enterSugar() //*****Start of enterSugar function*****
+{
+	//variables
+	int sugar;
+	int userAnswer;
+	ofstream outFile;
+	
+	cout << "(-1 to go back to the previous menu.)" << endl;
+	cout << "Enter blood sugar." << endl;
+	
+	while(userAnswer != -1)
+	{
+		outFile.open("bloodsugar.txt", ios::app);
+		if(outFile.is_open())
+		{
+			cout << "Blood Sugar: ";
+			cin >> sugar;
+			if(sugar == -1)
+			{
+				userAnswer = -1;
+				outFile.close();
+			} // end if
+			outFile << sugar << endl;
+			cout << "Added " << sugar << " to your blood sugar file." << endl << endl;
+		}// end if
+		else
+		cout << "File could not be opened." << endl;
+		
+		cout << "(-1 to go back to the previous menu.)" << endl;
+		cout << "Enter blood sugar." << endl;
+		outFile.close();
+	} //end while
+	
+}//*****End of enterSugar function*****
+
+
+
+
+void calcHemo()//*****Start of calcHemo function*****
+{
+	//variables for calcHemo function
+	double hemoglobin = 0.0;
+	int sugar = 0;
+	int userAnswer = 0;
+	int x = 0;
+	ifstream inFile;
+	
+	
+	cout << setprecision(2) << fixed;
+
+	
+	do
+	{
+		inFile.open("bloodsugar.txt", ios::in);		
+		if(inFile.is_open())
+		{
+						
+			while (!inFile.eof())
+			{
+				inFile >> sugar;
+				hemoglobin = hemoglobin + sugar;
+				x = x + 1;
+			}
+			hemoglobin = (hemoglobin - sugar) / (x -1);
+			if(hemoglobin <= 126)
+			{
+				cout << "Your approximate hemoglobin A1C is 6% or lower" << endl;
+			}
+			else if(hemoglobin > 126 && hemoglobin <= 154)
+			{
+				cout << "Your approximate hemoglobin A1C is 7%" << endl;
+			}
+			else if(hemoglobin > 154 && hemoglobin <= 183)
+			{
+				cout << "Your approximate hemoaglobin A1C is 8%" << endl;
+			}
+			else if(hemoglobin > 183 && hemoglobin <= 212)
+			{
+				cout << "Your approximate hemoglobin A1C is 9%" << endl;
+			}
+			else if(hemoglobin > 212 && hemoglobin <= 240)
+			{
+				cout << "Your approximate hemoglobin A1C is 10%" << endl;
+			}
+			else if(hemoglobin > 240 && hemoglobin <= 269)
+			{
+				cout << "Your approximate hemoglobin A1C is 11%" << endl;
+			}
+			else if(hemoglobin > 269 && hemoglobin <= 298)
+			{
+				cout << "Your approximate hemoglobin A1C is 12%" << endl;
+			}
+		}
+		else
+			cout << "File could not be opened." << endl;
+			cout << endl;			
+			cout << "(-1 to go back to previous menu.)" << endl;
+			cin >> userAnswer;
+			inFile.close();
+			
+	}while(userAnswer != -1);
+}//*****End of calcHemo function*****
+
+
+void calcSugar()//*****Start of calcSugar function*****
+{
+	//variables for calcSugar function
+	double hemoglobin = 0.0;
+	int sugar = 0;
+	int userAnswer = 0;
+	int x = 0;
+	ifstream inFile;
+	
+	
+	cout << setprecision(2) << fixed;
+	
+	do
+	{
+		inFile.open("bloodsugar.txt", ios::in);		
+		if(inFile.is_open())
+		{
+						
+			while (!inFile.eof())
+			{
+				inFile >> sugar;
+				hemoglobin = hemoglobin + sugar;
+				x = x + 1;
+			}
+			hemoglobin = (hemoglobin - sugar) / (x -1);
+			cout << "Your average blood sugar is: " << hemoglobin << endl;
+		}// end if
+		else
+			cout << "File could not be opened." << endl;
+			cout << endl;			
+			cout << "(-1 to go back to previous menu.)" << endl;
+			cin >> userAnswer;
+			inFile.close();
+			
+	}while(userAnswer != -1);
+}//*****End of calcSugar function*****
+
+void reviewSugar()//*****Start of reviewSugar function*****
+{
+	//variables for reviewSugar function
+	int sugar = 0;
+	int userAnswer = 0;
+	ifstream inFile;
+	
+
+	
+	do
+	{
+		inFile.open("bloodsugar.txt", ios::in);
+		if(inFile.is_open())
+		{
+			while(!inFile.eof())
+			{
+				inFile >> sugar;
+				if(!inFile) break;
+				cout << sugar << endl;
+
+			}
+			cout << "--------------" << endl;
+			cout << "End of list." << endl;
+		}// end if
+		else
+		cout << "File could not be opened." << endl;
+		cout << endl;
+		
+		cout << "(-1 to go back to previous menu.)" << endl;
+		cin >> userAnswer;
+		inFile.close();	
+		}while(userAnswer != -1);
+	
+}//*****End of reviewSugar function*****
+
+
+void onlineInfo() //*****Start of onlineInfo function*****
+{
+	int userAnswer = 0;
+	
+	cout << "(-1 to go back to the previous menu." << endl << endl;
+	cout << "All the websites on this list are classified as peer reviewed." << endl;
+	cout << "This means all the information has been tested and re-tested and then posted as accurately as possible." << endl;
+	cout << "Please make a choice on what information you want." << endl << endl;
+	cout << "Warning! Websites will automatically open." << endl << endl;
+	
+	
+	cout << "1: Blood Sugar." << endl;
+	cout << "2: Hemoglobin A1C." << endl;
+	cout << "3: Diet and exercise." << endl;
+	cout << "4: Type 1 diabetes." << endl;
+	cout << "5: Type 2 diabetes." << endl;
+	
+	cin >> userAnswer;
+	
+	
+	while(userAnswer != -1)
+	{
+		if(userAnswer == 1)
+		{
+			cout << "For more information on blood sugar we're taking you to:" << endl;
+			cout << "http://www.diabetes.org/living-with-diabetes/treatment-and-care/blood-glucose-control/" << endl;
+			system("open http://www.diabetes.org/living-with-diabetes/treatment-and-care/blood-glucose-control/");
+			
+		}
+		else if(userAnswer == 2)
+		{
+			cout << "For more information on your hemoglobin A1C we're taking you to:" << endl;
+			cout << "http://www.niddk.nih.gov/health-information/health-topics/diagnostic-tests/a1c-test-diabetes/Pages/index.aspx" << endl;
+			system("open http://www.niddk.nih.gov/health-information/health-topics/diagnostic-tests/a1c-test-diabetes/Pages/index.aspx");
+		}
+		else if(userAnswer == 3)
+		{
+			cout << "For more information on diet and exercise as a diabetic we're taking you to:" << endl;
+			cout << "http://www.diabetes.org/food-and-fitness/" << endl;
+			system("open http://www.diabetes.org/food-and-fitness/");
+		}
+		else if (userAnswer == 4)
+		{
+			cout << "For specific information on diabetes type 1 we're taking you to:" << endl;
+			cout << "http://www.diabetes.org/diabetes-basics/type-1/" << endl;
+			system("open http://www.diabetes.org/diabetes-basics/type-1/");
+		}
+		else if(userAnswer == 5)
+		{
+			cout << "For specific information on diabetes type 2 we're taking you to:" << endl;
+			cout << "http://www.diabetes.org/diabetes-basics/type-2/" << endl;
+			system("open http://www.diabetes.org/diabetes-basics/type-2/");
+		}//end if
+		else
+			cout << "What you entered is not in our system, please type one of the following." << endl;
+			cout << endl;
+			cout << "(-1 to go back to the previous menu.)" << endl;
+			cout << "Warning! Program will automatically open." << endl << endl;
+			cout << "1: Blood Sugar." << endl;
+			cout << "2: Hemoglobin A1C." << endl;
+			cout << "3: Diet and exercise." << endl;
+			cout << "4: Type 1 diabetes." << endl;
+			cout << "5: Type 2 diabetes." << endl;
+			
+			cin >> userAnswer;
+	}//end while
+	
+	
+}//*****End of onlineInfo function*****
